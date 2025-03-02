9f0f06ddc8ae57913cf0d5ec6de8c14ed2e1272b
SamuelDucca <samukaducca@hotmail.com>
2025-03-02 15:30:52 -0300

    Adding the ModelTraining module

---
+++ ModelTraining/preprocessing.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define SUBCARRIER_COUNT 52
+#define FRAME_WINDOW_SIZE 100
+#define PCA_COMPONENTS 42
+
+typedef struct {
+  float mean;
+  float std;
+} standard_scaler;
+
+extern const standard_scaler scaler[FRAME_WINDOW_SIZE][SUBCARRIER_COUNT];
+extern const float pca_means[FRAME_WINDOW_SIZE][SUBCARRIER_COUNT];
+extern const float pca_matrix[PCA_COMPONENTS][SUBCARRIER_COUNT * FRAME_WINDOW_SIZE];
